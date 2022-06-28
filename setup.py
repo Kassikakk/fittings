153f9fcb17d5ed65e422bc8706c4ba9a039b8c58
Kassikakk <52004537+Kassikakk@users.noreply.github.com>
2022-06-28 13:53:23 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='fittings',
+    version='1.0.0',    
+    description='Managers for some fitting cases using scipy.curve_fit',
+    packages=['fittings'],
+    install_requires=['numpy', 'scipy']
+                      )
